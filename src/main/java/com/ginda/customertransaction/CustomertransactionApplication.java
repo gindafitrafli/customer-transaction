52e571bff285ba613b54f28e915969330fca4471
gindafitrafli <gfitrafli@gmail.com>
2024-03-13 20:13:52 +0700

    project init

---
+++ src/main/java/com/ginda/customertransaction/CustomertransactionApplication.java
@@ -0,0 +1,13 @@
+package com.ginda.customertransaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomertransactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomertransactionApplication.class, args);
+	}
+
+}
